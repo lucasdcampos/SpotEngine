536e7e11b5fd1279ac21502f850960290d1db313
lucasof ☕ <lucasof.net@gmail.com>
2023-10-26 18:31:20 -0300

    New repository structure

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,14 +1,11 @@
-﻿// Copyright (c) Trivalent Studios
-// Licensed under MIT License.
-
-using SpotEngine;
+﻿using SpotEngine;
 
 public class Program
 {
     public static void Main(string[] args)
     {
         Application app = Application.GetApp();
+
         app.Run();
-        
     }
 }
