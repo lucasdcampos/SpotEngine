bacb084c4623c81a9660eee94e455049fa23af7a
lucasof ☕ <lucasof.net@gmail.com>
2023-10-27 14:56:28 -0300

    Create Rand class (Random)

---
+++ src/Math/Rand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotEngine
+{
+    public class Rand
+    {
+        public static float Between(float x, float y)
+        {
+            Random _rand = new Random();
+            double num = _rand.NextDouble() * (y - x) + x;
+
+            return (float)num;
+        }
+    }
+}
