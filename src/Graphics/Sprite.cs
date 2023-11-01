5e01740181f2d740cf11ab8267396f1dfaf63565
lucasof <lucazof@proton.me>
2023-10-31 23:33:35 -0300

    Add int layer to Sprite.cs

--- src/Graphics/Sprite.cs
+++ src/Graphics/Sprite.cs
@@ -8,13 +8,32 @@ namespace SpotEngine
 
         public Color color;
 
+        public int layer = 0;
+
         public Sprite()
         {
             texturePath = null;
             color = Color.White;
         }
 
-        public Sprite(string filePath, Color color)
+        public Sprite(FileStream filePath, Color color, int layer)
+        {
+            try
+            {
+                this.texturePath = new FileStream($"Assets/{filePath}", FileMode.Open);
+            }
+            catch (Exception)
+            {
+                Log.Error($"Asset 'Assets/{filePath}' could not be opended!");
+                return;
+            }
+
+            this.color = color == null ? Color.White : color;
+
+            this.layer = layer;
+        }
+
+       public Sprite(string filePath, Color color)
         {
             try
             {
