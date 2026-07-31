660461cd488333669fda841aa6e3503119d31c14
Lucas M. Campos <lucas@lucasof.com>
2026-07-30 23:55:57 -0300

    Rename SpotEngine file

---
+++ engine/SpotEngine.cs
@@ -0,0 +1,18 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Spot.Editor")]
+[assembly: InternalsVisibleTo("Spot.Engine.Tests")]
+
+namespace Spot;
+
+/// <summary>
+/// Provides top-level information about the Spot engine.
+/// </summary>
+public static class SpotEngine
+{
+    /// <summary>
+    /// Gets the current engine version.
+    /// </summary>
+    /// <returns>The engine version string.</returns>
+    public static string GetVersion() => "0.1.0";
+}
